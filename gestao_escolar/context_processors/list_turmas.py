9d12f6746af1b3578bf175422f110b23482099e2
Rogerio da Silva <rogerleonino@gmail.com>
2024-04-24 15:06:46 -0300

    melhoras

--- gestao_escolar/context_processors/list_turmas.py
+++ gestao_escolar/context_processors/list_turmas.py
@@ -3,6 +3,9 @@ from gestao_escolar.models import Turmas
 
 def list_turmas (request):
     contexto = {}
-    contexto ['turmas'] = Turmas.objects.all()
+    ano =  request.session['anoLetivo_id']
+    escola =  request.session['escola_id']
+
+    contexto ['turmas'] = Turmas.objects.filter(escola = escola, ano_letivo = ano)
 
     return contexto
